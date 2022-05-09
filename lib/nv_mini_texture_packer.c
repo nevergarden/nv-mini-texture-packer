4427b177f476d40c6677e9ed9cf3b6a9476ab693
nevergarden <nevergarden.fa@gmail.com>
2022-05-09 14:46:30 +0430

    fix: reading warning of fread not handling status

--- lib/nv_mini_texture_packer.c
+++ lib/nv_mini_texture_packer.c
@@ -7,7 +7,7 @@
 int nv_atlas_get_texture_count(const char* file_path) {
   int count = 0;
   FILE* fd = fopen(file_path, "rb");
-  fread(&count, sizeof(int), 1, fd);
+  if(fread(&count, sizeof(int), 1, fd) == 0) return -1;
   fclose(fd);
   return count;
 }
@@ -16,9 +16,12 @@ void nv_atlas_get_positions(nv_atlas_positions* positions,
                             const char* file_path) {
   FILE* file = fopen(file_path, "rb");
   int size = 0;
-  fread(&size, sizeof(int), 1, file);
+  if(fread(&size, sizeof(int), 1, file)==0){
+    fclose(file);
+    return;
+  }
   for (int i = 0; i < size; i++) {
-    fread(&positions[i], sizeof(nv_atlas_positions), 1, file);
+    if(fread(&positions[i], sizeof(nv_atlas_positions), 1, file) == 0) break;
   }
   fclose(file);
 }
